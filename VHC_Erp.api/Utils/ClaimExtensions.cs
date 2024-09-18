2e77b3bf4d4038dd5028d52e291373148d748400
Miki <vitorhugosnp@gmail.com>
2024-09-17 23:45:22 -0400

    feature: claim extensions added

--- VHC_Erp.api/Utils/ClaimExtensions.cs
+++ VHC_Erp.api/Utils/ClaimExtensions.cs
@@ -1,6 +1,13 @@
-﻿namespace VHC_Erp.api.Utils;
+﻿using System.Security.Claims;
 
-public class ClaimExtensions
+namespace VHC_Erp.api.Utils;
+
+public static class ClaimExtensions
 {
-    
+    public static string GetUsername(this ClaimsPrincipal user)
+    {
+        return user.Claims
+            .SingleOrDefault(x => x.Type.Equals("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname"))!
+            .Value;
+    }
 }
