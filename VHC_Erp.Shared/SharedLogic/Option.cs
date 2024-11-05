0e7a4dc25edf5c7e728ba88f4f747b66b24e352a
Miki <vitorhugosnp@gmail.com>
2024-11-04 21:55:29 -0400

    Changed the pattern of a failed api call response

--- VHC_Erp.Shared/SharedLogic/Option.cs
+++ VHC_Erp.Shared/SharedLogic/Option.cs
@@ -2,18 +2,19 @@
 
 namespace VHC_Erp.Shared.SharedLogic;
 
+
 public abstract record Option<T>{};
 
-public sealed record Some<T>(T Value, int StatusCode) : Option<T>;
-public sealed record None<T>(string Error, int ErrorCode) : Option<T>;
-
+public sealed record Some<T>(bool Success, T Value, int StatusCode, Metadata Metadata) : Option<T>;
+public sealed record None<T>(bool Sucess, string Error, int ErrorCode, Metadata Metadata) : Option<T>;
+public sealed record Metadata(DateTime TimeStamp, string version);
 public static class OptionExtensions
 {
-    public static Some<T> Some<T>(this T data) => new Some<T>(data, 200);
-    public static Some<T> Some<T>(this object data) => new Some<T>(data.Adapt<T>(), 200);
-    public static None<T> None<T>(this T _, string error) => new None<T>(error, 500);
-    public static None<T> None<T>(this T _, string error, int errorCode) => new None<T>(error, errorCode);
-    public static None<T> None<T>(this object _, string error) => new None<T>(error, 500);
-    public static None<T> None<T>(this  object _, string error, int errorCode) => new None<T>(error, errorCode);
+    public static Some<T> Some<T>(this T data) => new Some<T>(true, data, 200, new Metadata(DateTime.Now, "1.0"));
+    public static Some<T> Some<T>(this object data) => new Some<T>(true, data.Adapt<T>(), 200, new Metadata(DateTime.Now, "1.0"));
+    public static None<T> None<T>(this T _, string error) => new None<T>(false, error, 500, new Metadata(DateTime.Now, "1.0"));
+    public static None<T> None<T>(this T _, string error, int errorCode) => new None<T>(false, error, errorCode, new Metadata(DateTime.Now, "1.0"));
+    public static None<T> None<T>(this object _, string error) => new None<T>(false, error, 500, new Metadata(DateTime.Now, "1.0"));
+    public static None<T> None<T>(this  object _, string error, int errorCode) => new None<T>(false, error, errorCode, new Metadata(DateTime.Now, "1.0"));
     
 }
--- VHC_Erp.api/Utils/HandleEndpointResponse.cs
+++ VHC_Erp.api/Utils/HandleEndpointResponse.cs
@@ -8,12 +8,22 @@ public static class HandleEndpointResponse
     {
         return r.Exists ? Results.Json(r.Value, statusCode: r.HttpCode) : Results.Json(r.Error, statusCode:r.HttpCode);
     }
-    public static IResult HandleResponse<T>(this Option<T> r)
+    public static IResult HandleResponse<T>(this Option<T> res)
     {
-        return r switch
+        return res switch
         {
             Some<T> response => Results.Json(response.Value, statusCode: response.StatusCode),
-            None<T> response => Results.Json(response.Error.Split(["\n"], StringSplitOptions.None), statusCode: response.ErrorCode)
+            None<T> response => Results.Json(data: new 
+            {
+                success = response.Sucess,
+                error = new
+                {
+                    errorCode = response.ErrorCode,
+                    errorMessage = response.Error.Split(["\n"], StringSplitOptions.None)
+                },
+                metadata = response.Metadata 
+            }, statusCode: response.ErrorCode),
+            _ => Results.Problem("Unknown server problem.", statusCode: 500)
         };
     }
 }
